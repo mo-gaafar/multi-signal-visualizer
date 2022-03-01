83f53788f5ff9f040ec7c8a88a06294e249d4d06
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2022-03-01 19:48:20 +0200

    Started work on event connectors

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from layout import Ui_MainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
-import pyqtgraph
+from PyQt5 import QtWidgets, uic
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog, QScrollBar, QComboBox
 
-if __name__ == "__main__":
-    import sys
+from pyqtgraph import PlotWidget
+import pyqtgraph as pg
+import sys
+
+import interfacing  # local module
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('mainwindow2.ui', self)
+
+        interfacing.initConnectors(self)
+
+    def Browse(self):
+        self.filename = QFileDialog.getOpenFileName()
+        print(self.filename)
+
+    def ExportPDF(self):
+        self.filename = QFileDialog.getOpenFileName()
+        print(self.filename)
+        # @ABDULLAH etfaddal hena
+
+        # Step 1 choose folder location
+        # Step 2 create snapshot of plotter
+        # Step 3 create report with required variables and formatting
+        # Step 4 save in selected folder location (step 1)
+
+
+def main():
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    main = MainWindow()
+    main.show()
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+# BASIC CODE TO TEST WHETHER PYQTGRAPH WIDGET LOADS
